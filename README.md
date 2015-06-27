b7eb0e987a4f1f5bc15f5776094adc6b2f20a590
Onne Gorter <onne@onnlucky.com>
2015-06-27 17:32:41 +0200

    update example and readme

--- README.md
+++ README.md
@@ -1,43 +1,59 @@
 # An ESP8266 Arduino driver
 
-Still work in progress :)
+Status: works for reading/writing a single client TCP connection.
 
-The goal is to make a reasonably efficient driver. But also to never miss a
-single message. Most libraries will silently drop "+IPD" from the esp8266,
-when receiving one while doing other things.
+A efficient esp8266 driver for Arduino using the AT commands. Main feature is
+to never miss a data packet from the server.
 
 example:
 ```
 #include "esp8266.h"
+#include <SoftwareSerial.h>
+
+#define ESP_TX 3
+#define ESP_RX 2
 
 SoftwareSerial espconn(ESP_TX, ESP_RX);
-ESP8266 wifi(espconn, ESP_RESET);
-uint8_t buf[100];
+ESP8266 esp(espconn);
+uint8_t packetbuffer[100];
 
-setup() {
-    if (!wifi.joinAP("SSID", "PASS")) {
-        Serial.print("wifi error: ");
-        Serial.println(errno);
+void setup() {
+    espconn.begin(9600);
+    Serial.begin(9600);
+
+    if (!esp.reset()) {
+        Serial.println(F("wifi reset error"));
+        return;
     }
 
-    // setup a receive buffer, so we never have to miss a single message
-    wifi.putPacketBuffer(buf, sizeof(buf));
-
-    if (!wifi.tcpOpen("IP", PORT)) {
-        Serial.print("wifi error: ");
-        Serial.println(errno);
+    if (!esp.joinAP(SSID, PASS)) {
+        Serial.println(F("wifi join error"));
+        return;
     }
+    Serial.println("JOINED");
+
+    // setup the packet buffer here, so even if the server pushes data on connect, we get to read it
+    esp.putPacketBuffer(packetbuffer, sizeof(packetbuffer));
+
+    if (!esp.tcpOpen(IP, PORT)) {
+        Serial.println(F("tcp open error"));
+        return;
+    }
+    Serial.println("OPENED");
+
+    if (!esp.tcpSend((const uint8_t*)"1337\n", 5)) {
+        Serial.println(F("tcp write error"));
+        return;
+    }
+    Serial.println("WRITTEN");
 }
 
-loop() {
-    // if any bytes are available, take back the buffer, copy it to serial, give it back to the wifi driver
-    int len = wifi.available();
+void loop() {
+    int len = esp.available();
     if (len > 0) {
-        wifi.takePacketBuffer();
-        Serial.print("received: ");
-        Serial.write(buf, len);
-        wifi.putPacketBuffer(buf, sizeof(buf));
+        Serial.write(esp.takePacketBuffer(), len);
+        Serial.println();
+        esp.putPacketBuffer(packetbuffer, sizeof(packetbuffer));
     }
 }
 ```
-
--- example.ino
+++ example.ino
@@ -1,69 +1,55 @@
+#define SSID F("your-ssid")
+#define PASS F("your-password")
+#define HOST F("your-host")
+#define PORT 8080
+
 #include "esp8266.h"
 #include <SoftwareSerial.h>
-#include <errno.h>
 
 #define ESP_TX 3
 #define ESP_RX 2
-#define ESP_RESET 4
 
 SoftwareSerial espconn(ESP_TX, ESP_RX);
-ESP8266 esp(espconn, ESP_RESET);
+ESP8266 esp(espconn);
 uint8_t packetbuffer[100];
 
 void setup() {
-    Serial.begin(115200);
     espconn.begin(9600);
-
-    Serial.println(F("Hello World"));
+    Serial.begin(9600);
 
     if (!esp.reset()) {
         Serial.println(F("wifi reset error"));
-        if (!esp.reset()) {
-            Serial.println(F("wifi reset error"));
-            return;
-        }
-    }
-
-    if (!esp.joinAP(F("SSID"), F("PASS"))) {
-        Serial.print(F("wifi join error: "));
-        Serial.println(errno);
         return;
     }
-    Serial.println(F("JOINED"));
 
+    if (!esp.joinAP(SSID, PASS)) {
+        Serial.println(F("wifi join error"));
+        return;
+    }
+    Serial.println("JOINED");
+
+    // setup the packet buffer here, so even if the server pushes data on connect, we get to read it
     esp.putPacketBuffer(packetbuffer, sizeof(packetbuffer));
 
-    if (!esp.tcpOpen("192.168.87.101", 6979)) {
-        Serial.print(F("tcp open error: "));
-        Serial.println(errno);
+    if (!esp.tcpOpen(HOST, PORT)) {
+        Serial.println(F("tcp open error"));
         return;
     }
-    Serial.println(F("OPENED"));
+    Serial.println("OPENED");
 
     if (!esp.tcpSend((const uint8_t*)"1337\n", 5)) {
-        Serial.print(F("tcp write error: "));
-        Serial.println(errno);
+        Serial.println(F("tcp write error"));
         return;
     }
-
-    Serial.println(F("WRITTEN"));
+    Serial.println("WRITTEN");
 }
 
 void loop() {
-    while (true) {
-        int len = esp.available();
-        if (len > 0) {
-            Serial.write(esp.takePacketBuffer(), len);
-            esp.putPacketBuffer(packetbuffer, sizeof(packetbuffer));
-            Serial.println(F("munch munch"));
-        }
-    }
-
-    while (Serial.available()) {
-        espconn.write(Serial.read());
-    }
-    while (espconn.available()) {
-        Serial.write(espconn.read());
+    int len = esp.available();
+    if (len > 0) {
+        Serial.write(esp.takePacketBuffer(), len);
+        Serial.println();
+        esp.putPacketBuffer(packetbuffer, sizeof(packetbuffer));
     }
 }
 
