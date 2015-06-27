6c5a0ca622e0fe98c40197c33723398066d1bd04
Onne Gorter <onne@onnlucky.com>
2015-06-28 00:56:07 +0200

    report error code in example

--- example.ino
+++ example.ino
@@ -17,13 +17,16 @@ void setup() {
     espconn.begin(9600);
     Serial.begin(9600);
 
-    if (esp.reset()) {
-        Serial.println(F("wifi reset error"));
+    int s;
+    if ((s = esp.reset())) {
+        Serial.print(F("wifi reset error "));
+        Serial.println(s);
         return;
     }
 
-    if (esp.joinAP(SSID, PASS)) {
-        Serial.println(F("wifi join error"));
+    if ((s = esp.joinAP(SSID, PASS))) {
+        Serial.print(F("wifi join error"));
+        Serial.println(s);
         return;
     }
     Serial.println("JOINED");
@@ -31,14 +34,16 @@ void setup() {
     // setup the packet buffer here, so even if the server pushes data on connect, we get to read it
     esp.putPacketBuffer(packetbuffer, sizeof(packetbuffer));
 
-    if (esp.tcpOpen(HOST, PORT)) {
-        Serial.println(F("tcp open error"));
+    if ((s = esp.tcpOpen(HOST, PORT))) {
+        Serial.print(F("tcp open error"));
+        Serial.println(s);
         return;
     }
     Serial.println("OPENED");
 
-    if (esp.tcpSend((const uint8_t*)"1337\n", 5)) {
-        Serial.println(F("tcp write error"));
+    if ((s = esp.tcpSend((const uint8_t*)"1337\n", 5))) {
+        Serial.print(F("tcp write error"));
+        Serial.println(s);
         return;
     }
     Serial.println("WRITTEN");
@@ -50,6 +55,9 @@ void loop() {
         Serial.write(esp.takePacketBuffer(), len);
         Serial.println();
         esp.putPacketBuffer(packetbuffer, sizeof(packetbuffer));
+    } else if (len < 0) {
+        Serial.print(F("tcp receive error"));
+        Serial.println(len);
     }
 }
 
